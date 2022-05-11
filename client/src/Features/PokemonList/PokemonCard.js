dad2d0ad9e3b8950c90a92604619682479f91b09
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-05-11 00:15:10 -0400

    adding loading function and getting api call details to rendewr

--- client/src/Features/PokemonList/PokemonCard.js
+++ client/src/Features/PokemonList/PokemonCard.js
@@ -1,27 +1,39 @@
 import axios from 'axios';
+import { useEffect } from 'react';
 import { Card, Button } from 'react-bootstrap';
 import { useState } from 'react';
 
 function PokemonCard (){
-    let [pokemon, setPokemon ] = useState();
+    const [pokemon, setPokemon] = useState();
+    const [loading, setLoading] = useState(true);
 
-    axios.get("https://pokeapi.co/api/v2/pokemon/pikachu/")
-        .then(res => setPokemon(res))
-        .catch(error => console.log(error))
-        console.log(pokemon);
+    useEffect(() => {
+      axios.get("https://pokeapi.co/api/v2/pokemon/pikachu/")
+      .then( res => setPokemon(res.data))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
+    }, [])
+
+    console.log(pokemon)
+    
       return(
-        <Card style={{ width: '18rem' }}>
+        <>
+        {loading ? (<h2>loading...</h2>) : (
+            <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
                 <Card.Title>Card Title</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                  {pokemon.name}
+                  {pokemon.types[0].type.name}
                 </Card.Text>
                 <Button variant="primary">Go somewhere</Button>
             </Card.Body>
-        </Card> 
+            </Card> 
+        )}
+        </>
       )
 }
 
 export default PokemonCard;
+
