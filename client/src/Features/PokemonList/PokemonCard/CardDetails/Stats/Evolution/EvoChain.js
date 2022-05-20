a16f9ac4e45544cd9a08cb5c63eb7645b199ceae
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-05-20 01:03:38 -0400

    images and names are loading properly on evolution tab. Finally!

--- client/src/Features/PokemonList/PokemonCard/CardDetails/Stats/Evolution/EvoChain.js
+++ client/src/Features/PokemonList/PokemonCard/CardDetails/Stats/Evolution/EvoChain.js
@@ -5,74 +5,86 @@ import { Card } from "react-bootstrap";
 
 function EvoChain({evoChain}) {
     const [loading, setLoading] = useState(true);
+    let [imageUrl1, setUrl1] = useState();
+    let [imageUrl2, setUrl2] = useState();
+    let [imageUrl3, setUrl3] = useState();
 
-    let [evo1, setEvo1] = useState({name: null, imageUrl: null})
-    let [evo2, setEvo2] = useState({name: null, imageUrl: null})  
-    let [evo3, setEvo3] = useState({name: null, imageUrl: null})      
+
+    let evo1;
+    let evo2;
+    let evo3;
+
+
+    evo1 = evoChain.species.name;
+
+    if(evoChain.evolves_to[0].length !=0 ){
+        evo2= evoChain.evolves_to[0].species.name
+    }
+    if(evoChain.evolves_to[0].evolves_to.length != 0){
+        evo3= evoChain.evolves_to[0].evolves_to[0].species.name
+    }
+
+
 
     useEffect(() =>{
-        setEvo1( 
-            {name: evoChain.species.name}
-        )
-        if(evoChain.evolves_to[0].length !=0 ){
-            setEvo2(
-                {name:evoChain.evolves_to[0].species.name}
-                )
-        }
-        if(evoChain.evolves_to[0].evolves_to.length != 0){
-            setEvo3(
-                {name:evoChain.evolves_to[0].evolves_to[0].species.name}
-            )
-        }
 
-        const getEvoImages = async () => {
-            let url = 'https://pokeapi.co/api/v2/pokemon/'
-            console.log(evo1.name)
-            let poke1Data = await axios.get(url + evo1.name);
-            setEvo1(
-                {imageUrl: poke1Data.data.sprites.front_default}
-            )
+        let url = 'https://pokeapi.co/api/v2/pokemon/'
+
+        const getImageUrls = async () => {
+            let poke1Data = await axios.get(url + evo1);
+            setUrl1(poke1Data.data.sprites.front_default) 
+            
+            if(evo2 != null){
+                let poke2Data = await axios.get(url + evo2);
+                setUrl2(poke2Data.data.sprites.front_default);
+            }
+    
+            if(evo3 != null){
+                let poke3Data = await axios.get(url + evo3);
+                setUrl3(poke3Data.data.sprites.front_default);
+            }
         }
-
-        getEvoImages();
-
+         
+        getImageUrls()
+        .catch((err) => console.log(err))
+        setLoading(false)
     }, [])
 
     
-    
+ 
 
    
-    //console.log(evoChain)
-    
-    //getEvoImages();
     
     
-
+    
+    
+    
+    
 
     if(loading){
         return(<h3>loading</h3>)
     }
 
-       console.log(evo3)
+    
 
     return(
         <>
             <Card>
-                <Card.Img variant="top" src={evo1.imageUrl} />
+                <Card.Img variant="top" src={imageUrl1} />
                 <Card.Body>
-                <Card.Title>{evo2.name}</Card.Title>
+                <Card.Title>{evo1}</Card.Title>
                 </Card.Body>
             </Card>
             <Card>
-                <Card.Img variant="top" src={evo2.imageUrl} />
+                <Card.Img variant="top" src={imageUrl2} />
                 <Card.Body>
-                <Card.Title>{evo2.name}</Card.Title>
+                <Card.Title>{evo2}</Card.Title>
                 </Card.Body>
             </Card>
             <Card>
-                <Card.Img variant="top" src={evo3.imageUrl} />
+                <Card.Img variant="top" src={imageUrl3} />
                 <Card.Body>
-                <Card.Title>{evo3.name}</Card.Title>
+                <Card.Title>{evo3}</Card.Title>
                 </Card.Body>
             </Card>
         
