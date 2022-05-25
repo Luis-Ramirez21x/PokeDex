ce05d3dbf93784768cd23861d97306ee08581be2
Luis Ramirez <luis.ramirez21x@outlook.com>
2022-05-25 17:35:50 -0400

    added star state to allow user to save a pokmeon, still need to add a post method for it

--- client/src/Features/PokemonList/PokemonCard/CardHeader/CardHeader.js
+++ client/src/Features/PokemonList/PokemonCard/CardHeader/CardHeader.js
@@ -1,8 +1,18 @@
 import { Card, Col, Container, Row, Badge } from "react-bootstrap";
 import './CardHeader.css';
+import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 
 function CardHeader({imgUrl, name, types}){
+
+    //star pokemon state 
+    const [stared, starPokemon] = useState(true);
+    //const stars = Array(1).fill(0)
+
+    function savePokemon(){
+        starPokemon(!stared);
+    }
     
 
     return(
@@ -12,6 +22,10 @@ function CardHeader({imgUrl, name, types}){
                     <Card className="header">
                         <Card.Body>
                         <Card.Title>{name}</Card.Title>
+                        <FaStar key={'star'} 
+                        size={24} color={stared ? "#FFBA5A" : "#a9a9a9"} 
+                        style={{marginRight: 10,}}
+                        onClick={savePokemon}/>
                         <Card.Text>
                             {types.map((type) => {
                                 return(
